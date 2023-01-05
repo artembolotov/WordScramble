fe9580245c8d8630f8ca83af70ebe529e64bfc37
Artem Bolotov <teeemon@gmail.com>
2023-01-05 17:02:27 +0300

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by artembolotov on 05.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
